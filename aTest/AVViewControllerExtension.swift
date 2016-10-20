c38c0799f60c2ea9ae210eb7bb9f61630013b08a
Francesco Thiery <francescothiery@imac-di-francesco.local>
2016-10-20 23:19:38 +0200

    Fix

---
+++ aTest/AVViewControllerExtension.swift
@@ -0,0 +1,18 @@
+//
+//  AVViewControllerExtension.swift
+//  DeltaPlay
+//
+//  Created by Francesco Thiery on 20/10/2016.
+//  Copyright © 2016 Coocked. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+extension ViewController : AVAudioSessionDelegate{
+    
+    
+//    func foo(){
+//        AVAudioSession.sharedInstance().delegate = self
+//    }
+}
--- aTest/ViewController.swift
+++ aTest/ViewController.swift
@@ -15,22 +15,21 @@ class ViewController: ADTableViewController {
     
     var player: AVAudioPlayer?
     var shareButton : UIBarButtonItem!
+    var isSelecting : Bool = true
+    var fakeBeanList = CellBean.getBeans()
     
     override func adViewDidLoad() {
         super.adViewDidLoad()
         self.tableView.delegate = self
         self.title = "DeltaPlay"
-        let addButtonImageView = UIImageView(image: UIImage(named: "share"))
-        //shareButton = UIBarButtonItem(customView: addButtonImageView)
         shareButton = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(ViewController.share))
-        //shareButton.customView = addButtonImageView
-        //        shareButton.target = self
-        //        shareButton.action = #selector(share)
         self.navigationItem.rightBarButtonItem = shareButton
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Annulla", style: .done, target: self, action: #selector(ViewController.seleziona))
+        self.navigationItem.leftBarButtonItem?.setTitleTextAttributes( [NSFontAttributeName : UIFont.bebasNeueFont(ofSize: 16)!], for: .normal)
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Ti vogliamo bene Andrè"
+        return "@deltaforce76"
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -54,9 +53,16 @@ class ViewController: ADTableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        fakeBeanList[indexPath.row].selected = !fakeBeanList[indexPath.row].selected
-        tableView.reloadData()
-        playSound(name: fakeBeanList[indexPath.row].audioTitle)
+        
+        if !fakeBeanList[indexPath.row].selected{
+            playSound(name: fakeBeanList[indexPath.row].audioTitle)
+        }
+        
+        if isSelecting{
+            fakeBeanList[indexPath.row].selected = !fakeBeanList[indexPath.row].selected
+            tableView.reloadData()
+        }
+        
     }
     
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -93,5 +99,22 @@ class ViewController: ADTableViewController {
         }
     }
     
+    func seleziona(){
+        if isSelecting{
+            
+            DispatchQueue.main.async {
+                for bean in self.fakeBeanList{
+                    bean.selected = false
+                }
+                (self.view as? UITableView)?.reloadData()
+            }
+            
+            self.navigationItem.leftBarButtonItem?.title = "Seleziona"
+        }else{
+            self.navigationItem.leftBarButtonItem?.title = "Annulla"
+        }
+        
+        isSelecting = !isSelecting
+    }
 }
 
